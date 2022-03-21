2429a3924f29b7c14ab9d357fb8a8d72d84a204a
Alberto Peñalba Martos <albpenal@ucm.es>
2022-03-21 11:25:26 +0100

    Archivos iniciales añadidos al indice del repositorio

---
+++ Practica4albjos/Practica4albjos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica4albjos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
